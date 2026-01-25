48a2ee657c40849cbb9f0d6fda72053f09eb2ac9
Afrina Tithy <afrina.tithy@gmail.com>
2026-01-25 22:16:08 +0600

    Implement responsive navbar using Bootstrap

---
+++ components/Navbar.js
@@ -0,0 +1,44 @@
+"use client";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light border-bottom sticky-top">
+            <div className="container">
+                <Link href="/" className="navbar-brand fw-bold text-primary">
+                    ShopEase
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target=".navbarNav"
+                >
+                    <span className="navbar-toggler-icon"></span>
+
+                </button>
+
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-3">
+                        <li className="nav-item">
+                            <Link href="/" className="nav-link fw-medium" >Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/about" className="nav-link fw-medium" >About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/products" className="nav-link fw-medium" >Products</Link>
+                        </li>
+
+                    </ul>
+
+                    <div className="d-flex gap-2">
+                        <Link href="/login" className="btn btn-outline-primary">Login</Link>
+                        <Link href="/login" className="btn btn-outline-primary">Signup</Link>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
